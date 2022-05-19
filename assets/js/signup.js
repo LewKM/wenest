f613abc2976807fc74682a14e3e563606e3d1058
carlton <carltonmbaj0@gmail.com>
2022-05-19 15:54:00 +0300

    Added authentication to login

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -9,21 +9,16 @@ setInterval(() => {
     document.getElementById("submit").removeAttribute("disabled");
   }
 });
-
 function check(form) {
   if (form.username.value && form.password.value) {
-    // alert("Welcome to Wenest");
     var username = document.getElementById("username").value;
-    // alert("Welcome " + username);
-    // document.write("Welcome " + username);
   } else {
     alert("Error Password or Username");
   }
 }
 document.getElementById("submit").onclick = function () {
   window.location.href = "./assets/home.html";
-  // alert("Welcome " + $, { username });
-  // alert("Account creted successfully");
+
   var username = document.getElementById("username").value;
 
   alert("Welcome" + " " + username);
